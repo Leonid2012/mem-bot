6f4c5941bfcfb04ee1984c4b43c9b262acbeced5
Leonid2012 <151195803+Leonid2012@users.noreply.github.com>
2023-12-22 09:05:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+@bot.command()
+async def mem(ctx):
+    with open('images/mem1.jpg', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)   
+
+bot.run("MTE4MDE3OTE0ODU4OTc4MTAyMw.GpwbB-.abI1D8m_UrmQCABlc5sbU_Dj2NrThwGvPuw2Dk")
